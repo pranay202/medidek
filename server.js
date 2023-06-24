2baa08ba37e6c23b88b79d5cfac406d82cc9bc96
swag-pranay <pranay@swagstay.in>
2023-06-24 20:09:58 +0530

    deployment

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const colors = require("colors");
-const moragan = require("morgan");
-const dotenv = require("dotenv");
-const connectDB = require("./config/db");
+const express = require('express');
+const colors = require('colors');
+const moragan = require('morgan');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 //dotenv conig
 dotenv.config();
@@ -15,12 +15,19 @@ const app = express();
 
 //middlewares
 app.use(express.json());
-app.use(moragan("dev"));
+app.use(moragan('dev'));
 
 //routes
-app.use("/api/v1/user", require("./routes/userRoutes"));
-app.use("/api/v1/admin", require("./routes/adminRoutes"));
-app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
+app.use('/api/v1/user', require('./routes/userRoutes'));
+app.use('/api/v1/admin', require('./routes/adminRoutes'));
+app.use('/api/v1/doctor', require('./routes/doctorRoutes'));
+
+//static files
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
 
 //port
 const port = process.env.PORT || 8080;
